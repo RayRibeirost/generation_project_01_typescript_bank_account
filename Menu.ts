02113f2b29c50697a7abcd493c4803f9ee5ded07
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2025-12-05 16:41:53 -0300

    update: bank account app Colors class document created; second part of the project completed and tested

--- Menu.ts
+++ Menu.ts
@@ -1,9 +1,10 @@
 import readline from "readline-sync";
+import { colors } from "./src/util/Colors";
 
 export function main() {
     let option: number;
     while(true) {
-        console.log("***************************************************************");
+        console.log(colors.bg.black, colors.fg.yellow, "***************************************************************");
         console.log("                                                               ");
         console.log("                    Banco do Brazil com Z                      ");
         console.log("                                                               ");
@@ -19,56 +20,72 @@ export function main() {
         console.log("                 8 - Transferir valores entre Contas           ");
         console.log("                 9 - Sair                                      ");
         console.log("                                                               ");
-        console.log("***************************************************************");
+        console.log("***************************************************************", colors.reset);
 
         option = readline.questionInt("Entre com a opção desejada: ");
 
         if(option === 9) {
-            console.log("\nBanco do Brazil com Z - O seu futuro começa aqui;");
+            console.log(colors.fg.greenstrong, "\nBanco do Brazil com Z - O seu futuro começa aqui;");
+            console.log("", colors.reset)
             sobre();
             process.exit(0);            
         }
 
         switch(option) {
             case 1:
-                console.log("\n\nCriar Conta\n\n");
+                console.log(colors.fg.whitestrong, "\n\nCriar Conta\n\n", colors.reset);
+                keyPress();
                 break;
             case 2:
-                console.log("\n\nListar todas as Contas\n\n");
+                console.log(colors.fg.whitestrong, "\n\nListar todas as Contas\n\n", colors.reset);
+                keyPress();
                 break;
             case 3:
-                console.log("\n\nConsultar dados da conta - por número\n\n");
+                console.log(colors.fg.whitestrong, "\n\nConsultar dados da conta - por número\n\n", colors.reset);
+                keyPress();
                 break;
             case 4:
-                console.log("\n\nAtualizar dados da Conta\n\n");
+                console.log(colors.fg.whitestrong, "\n\nAtualizar dados da Conta\n\n", colors.reset);
+                keyPress();
                 break;
             case 5:
-                console.log("\n\nApagar uma Conta\n\n");
+                console.log("\n\nApagar uma Conta\n\n", colors.reset);
+                keyPress();
                 break;
             case 6:
-                console.log("\n\nSaque\n\n");
+                console.log(colors.fg.whitestrong, "\n\nSaque\n\n", colors.reset);
+                keyPress();
                 break;
             case 7:
-                console.log("\n\nDepósito\n\n");
+                console.log(colors.fg.whitestrong, "\n\nDepósito\n\n", colors.reset);
+                keyPress();
                 break;
             case 8:
-                console.log("\n\nTransferência entre Contas\n\n");
+                console.log(colors.fg.whitestrong, "\n\nTransferência entre Contas\n\n", colors.reset);
+                keyPress();
                 break;
             default:
-                console.log("\n\nOpção inválida!\n\n");
+                console.log(colors.fg.whitestrong, "\n\nOpção inválida!\n\n", colors.reset);
+                keyPress();
                 break;
         }
     }
 }
 
-export function sobre(): void {
+function sobre(): void {
     console.log("\n***************************************************************");
     console.log("Projeto desenvolvido por: Raylander Ribeiro (rayribeirost)");
     console.log("Generation Brasil - generation@generation.com");
     console.log("github.com/rayribeirost")
     console.log("***************************************************************");
 
-}
+};
+
+function keyPress():void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...")
+    readline.prompt();
+};
 
 main();
 
