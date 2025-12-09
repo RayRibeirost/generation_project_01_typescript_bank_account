c5e21f1c6f664dd3b3a96137d2c0c0a6f3e4a185
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2025-12-09 11:02:24 -0300

    feat: Savings account class created

--- src/model/SavingsAccount.ts
+++ src/model/SavingsAccount.ts
@@ -1,1 +1,37 @@
 import { Account } from "./Account";
+
+export class CheckingAccount extends Account {
+    
+    private _anniversary: number;
+
+    constructor (
+        accountNumber: number,
+        bankBranch: number,
+        accountType: number,
+        accountHolder: string,
+        balance: number,
+        anniversary: number,
+    ) {
+        super(accountNumber, bankBranch, accountType, accountHolder, balance);
+        this._anniversary = anniversary;
+    };
+
+    // getters
+    
+    public get anniversary(): number {
+        return this._anniversary;
+    };
+
+    // setters
+
+    public set anniversary(limit: number) {
+        this._anniversary = limit;
+    }
+
+    // methods
+
+    public viewAccount(): void {
+        super.viewAccount();
+        console.log(`Dia do aniversário: ${this._anniversary.toFixed(2)}`); 
+    }
+}
