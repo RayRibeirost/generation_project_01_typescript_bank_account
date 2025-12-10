41a961a6edf2088f4d4584c15985bba9dc69562b
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2025-12-10 15:13:34 -0300

    feat: add withdraw method in tha account controller

--- src/controller/AccountController.ts
+++ src/controller/AccountController.ts
@@ -25,7 +25,12 @@ export class AccountController implements AccountRepository {
         throw new Error("Method not implemented.");
     }
     withdraw(accNumber: number, amount: number): void {
-        throw new Error("Method not implemented.");
+        let account = this.searchInCollection(accNumber);
+        if (account != null) {
+            if(account.withdraw(amount) == true) console.log(colors.fg.green, `\nO saque na conta ${accNumber} foi efetuado com sucesso`, colors.reset);
+        } else {
+            console.log(colors.fg.red, `\nA conta número: ${accNumber} não foi encontrada!`, colors.reset);
+        }
     }
     deposit(accNumber: number, amount: number): void {
         throw new Error("Method not implemented.");
