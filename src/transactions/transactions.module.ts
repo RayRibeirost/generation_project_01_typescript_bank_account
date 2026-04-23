827cd85d643ac305338a43e212c7dd4641365098
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2026-04-22 21:42:21 -0300

    feat: create transactions module

---
+++ src/transactions/transactions.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Transactions } from "./entities/transaction.entity";
+import { Accounts } from "../accounts/entities/account.entity";
+import { TransactionService } from "./services/transactions.service";
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([Transactions, Accounts])
+    ],
+    providers: [TransactionService],
+    exports: [TransactionService],
+})
